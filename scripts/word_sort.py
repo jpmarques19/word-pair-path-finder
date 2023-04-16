11338f4e1515f48a4d41c880b6a163262b86f176
jpmarques19 <jpmarques19@gmail.com>
2023-04-16 17:06:39 +0100

    Create word_sort.py

---
+++ scripts/word_sort.py
@@ -0,0 +1,7 @@
+words = ["casa", "cosa", "cato", "cima", "pala", "palo", "papa", "pata", "pato", "pima", "pita", "pito", "pota", "poto", "tapa", "tata", "tima", "tita", "tito", "tota", "toto"]
+
+# Sort the list
+words.sort()
+
+# Print the sorted list
+print(words)
