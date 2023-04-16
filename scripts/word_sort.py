aaae2ee288666dd20902b57fcd57d3ecc4c38c93
jpmarques19 <jpmarques19@gmail.com>
2023-04-16 17:25:52 +0100

    Implement word_distance
    This reverts commit 11338f4e1515f48a4d41c880b6a163262b86f176.

--- scripts/word_sort.py
+++ scripts/word_sort.py
@@ -7,5 +7,15 @@ words.sort()
 print(words)
 
 
-def word_distance():
-    pass
+def word_distance(word1, word2):
+    """
+    calculate the distance between two words
+    """
+    if len(word1) != len(word2):
+        return -1
+    
+    distance = 0
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            distance += 1
+    return distance
