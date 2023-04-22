58fb4cf82200cc4c3d82d392a59a097d7730c645
jpmarques19 <jpmarques19@gmail.com>
2023-04-22 14:30:22 +0100

    Use more efficient Trie

--- scripts/create_adjacency_list.py
+++ scripts/create_adjacency_list.py
@@ -1,42 +1,5 @@
 from collections import defaultdict
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word_indices = []
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word, index):
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.word_indices.append(index)
-
-    def search(self, word, max_distance):
-        candidates = []
-
-        stack = [(self.root, 0, 0, False)]  # node, index, distance, visited
-
-        while stack:
-            node, index, distance, visited = stack.pop()
-
-            if index == len(word) and distance <= max_distance:
-                candidates.extend(node.word_indices)
-
-            if not node.children or distance > max_distance:
-                continue
-
-            if not visited:
-                stack.append((node, index + 1, distance, True))
-                for char, child in node.children.items():
-                    stack.append((child, index + 1, distance + (char != word[index]), False))
-
-        return candidates
+import datrie
 
 def adicionaAresta(grafo,u,v):
     grafo[u].append(v)
@@ -51,7 +14,6 @@ def word_distance(word1, word2):
 
     return sum(char1 != char2 for char1, char2 in zip(word1, word2))
 
-
 def create_adjacency_list(words):
     grafo = [[] for _ in range(len(words))]
 
@@ -60,17 +22,24 @@ def create_adjacency_list(words):
     for idx, word in enumerate(words):
         words_by_length[len(word)].append((idx, word))
 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     for word_indices in words_by_length.values():
-        trie = Trie()
+        trie = datrie.Trie(alphabet)
         for idx, word in word_indices:
-            trie.insert(word, idx)
+            trie[word] = idx
 
         for idx1, word1 in word_indices:
-            candidates = trie.search(word1, max_distance=1)
-            for idx2 in candidates:
-                if idx1 != idx2 and word_distance(word1, words[idx2]) == 1:
-                    adicionaAresta(grafo, idx1, idx2)
+            for i in range(len(word1)):
+                for char in alphabet:
+                    candidate_word = word1[:i] + char + word1[i+1:]
+                    if candidate_word in trie:
+                        idx2 = trie[candidate_word]
+                        if idx1 != idx2 and word_distance(word1, words[idx2]) == 1:
+                            adicionaAresta(grafo, idx1, idx2)
 
     return grafo
+
+
 
 
